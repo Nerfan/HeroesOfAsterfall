ad43138b9c69d419687431d9cbbdf637345a96e1
jeremy <jerelefurge@gmail.com>
2016-06-03 13:40:16 -0400

    Beta Version 3.1

--- src/GUI.java
+++ src/GUI.java
@@ -3,7 +3,6 @@ import Units.Player;
 import Units.Weapon;
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
